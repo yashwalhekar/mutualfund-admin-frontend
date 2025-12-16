fabb123fcf2e490cb469cfe454730e14033b80c1
yashwalhekar <yashwalhekar371@gmail.com>
2025-12-16 14:36:13 +0530

    sidebar

--- src/components/dashboard/StatCards.jsx
+++ src/components/dashboard/StatCards.jsx
@@ -23,7 +23,9 @@ export const StatCards = ({ stats }) => {
         <Tooltip key={i} title={item.desc} arrow placement="top">
           <div className="bg-gradient-to-r from-[#444F87] to-[#4F5CA0] text-white p-4 sm:p-5 rounded-xl shadow-md text-center transition-all hover:scale-105 cursor-pointer">
             {/* Label */}
-            <div className="text-xs sm:text-sm opacity-90">{item.label}</div>
+            <div className="text-sm sm:text-base md:text-lg opacity-90">
+              <p>{item.label}</p>
+            </div>
 
             {/* Count */}
             <div className="text-xl sm:text-2xl font-extrabold mt-1">
