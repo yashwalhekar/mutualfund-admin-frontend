bb98e85a8b6e6c85c12c756814f9423cdb89e591
yashwalhekar <yashwalhekar371@gmail.com>
2025-12-16 15:53:35 +0530

    Update the ui of dashboard and change the image of login page

--- src/components/dashboard/StatCards.jsx
+++ src/components/dashboard/StatCards.jsx
@@ -14,23 +14,37 @@ export const StatCards = ({ stats }) => {
       count: stats.testimonials,
       desc: "Total client testimonials received",
     },
-    { label: "Users", count: stats.users, desc: "Total Reached users" },
+    {
+      label: "Users",
+      count: stats.users,
+      desc: "Total Reached users",
+    },
   ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
+    <div className="grid grid-cols-3 gap-2 sm:gap-4 mb-6">
       {cardData.map((item, i) => (
         <Tooltip key={i} title={item.desc} arrow placement="top">
-          <div className="bg-gradient-to-r from-[#444F87] to-[#4F5CA0] text-white p-4 sm:p-5 rounded-xl shadow-md text-center transition-all hover:scale-105 cursor-pointer">
+          <div
+            className="bg-gradient-to-r from-[#444F87] to-[#4F5CA0] 
+            text-white 
+            px-2 py-3 sm:px-4 sm:py-4 
+            rounded-lg sm:rounded-xl 
+            shadow-md 
+            text-center 
+            transition-all 
+            hover:scale-105 
+            cursor-pointer"
+          >
             {/* Label */}
-            <div className="text-sm sm:text-base md:text-lg opacity-90">
-              <p>{item.label}</p>
-            </div>
+            <p className="text-[11px] sm:text-sm md:text-base opacity-90 truncate">
+              {item.label}
+            </p>
 
             {/* Count */}
-            <div className="text-xl sm:text-2xl font-extrabold mt-1">
+            <p className="text-lg sm:text-2xl md:text-3xl font-extrabold mt-1">
               {item.count}
-            </div>
+            </p>
           </div>
         </Tooltip>
       ))}
