220f9248fca2cd6528eee800f2c80793c5306ba6
yashwalhekar <yashwalhekar371@gmail.com>
2025-12-29 19:31:44 +0530

    add the chatbot questions and update the blogs

---
+++ src/app/(adminLayout)/chatbot/add/page.jsx
@@ -0,0 +1,12 @@
+import AddQuestions from "@/components/chatbot/AddQuestions";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <AddQuestions />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/(adminLayout)/chatbot/page.jsx
@@ -0,0 +1,12 @@
+import ViewQuestions from "@/components/chatbot/ViewQuestions";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <ViewQuestions />
+    </div>
+  );
+};
+
+export default page;
